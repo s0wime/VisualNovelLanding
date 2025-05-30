7c5bca6704fb0ab2d2b59ca61a1f2fdc5f6693c1
Vitalii <158774939+s0wime@users.noreply.github.com>
2025-05-30 13:35:48 +0300

    feat(visitor): add visitor uuid validation

--- controllers/visitorsController.js
+++ controllers/visitorsController.js
@@ -8,6 +8,12 @@ class VisitorsController {
       return res.status(400).json({ error: "Bad request." });
     }
 
+    const uuidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    if (!uuidRegex.test(visitorId)) {
+      return res.status(400).json({ error: "Bad request." });
+    }
+
     try {
       await VisitorsService.addVisitor(visitorId);
     } catch (e) {
